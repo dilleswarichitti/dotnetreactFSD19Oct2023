2febdc9defd9e4db7f821a205b4d036f2a229b1d
Chitti <dilleswarichitti@gmail.com>
2023-11-15 16:17:40 +0530

    web

---
+++ HotelAPISol/HotelAPI/Models/Hotel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelAPI.Models
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public List<Room> Rooms { get; set; }
+    }
+}
---
+++ HotelAPISol/HotelAPI/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelAPI.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomNumber { get; set; }
+        public int HotelId { get; set; }
+        [ForeignKey("HotelId")]
+        public Hotel Hotel { get; set; }
+    }
+}
