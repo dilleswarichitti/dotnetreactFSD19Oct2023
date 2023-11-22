181f49123a6156295aa5677aff269d36a3477bee
Chitti <dilleswarichitti@gmail.com>
2023-11-22 19:53:49 +0530

    Day18

---
+++ React/Day18/first-app/src/components/Second.js
@@ -0,0 +1,11 @@
+import './Second.css';
+      
+function Second(){
+    return(
+        <div className='second'>
+            <h1>Second Component</h1>
+        </div>
+    );
+}
+
+export default Second;
