7ce3bb2c07184e314334338a21b627851eced2b8
Chitti <dilleswarichitti@gmail.com>
2023-11-25 22:29:02 +0530

    react

--- ShoppingSol/React/Day18/first-app/src/App.js
+++ ShoppingSol/React/Day18/first-app/src/App.js
@@ -1,31 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import AddProduct from './components/AddProduct';
-import Products from './components/Products';
+import RegisterUser from './components/RegisterUser';
 
 function App() {
-  var scores = [90,100,56,89,73];
+  //var scores = [90,100,56,89,73];
   return (
-    <div className="App">
-          <div className="container text-center">
-        <div className="row">
-          <div className="col">
-            <Products/> 
-          </div>
-          <div className="col">
-            <AddProduct/>
-          </div>
-        </div>
-    </div>
+   //<div className="App">
+    //       <div className="container text-center">
+    //     <div className="row">
+    //       <div className="col">
+    //         <Products/> 
+    //       </div>
+    //       <div className="col">
+    //         <AddProduct/>
+    //       </div>
+    //     </div>
+    // </div>
+    //   <div>
+    //       {scores.map((score)=>
+    //         <li key={score}>{score}</li>
+    //       )}
+    //   </div>
       <div>
-          {scores.map((score)=>
-            <li key={score}>{score}</li>
-          )}
+        <RegisterUser/>
       </div>
-      <div>
-        
-      </div>
-    </div>
   );
 }
 
--- ShoppingSol/ShoppingApp/Controllers/CustomerController.cs
+++ ShoppingSol/ShoppingApp/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShoppingApp.Interface;
@@ -8,6 +9,7 @@ namespace ShoppingApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("reactApp")]
     public class CustomerController : ControllerBase
     {
         private readonly IUserService _userService;
